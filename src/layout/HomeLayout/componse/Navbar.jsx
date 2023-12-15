6566ff8123e4603c28e5860c789c3f7a60f22ab6
kakuSun <lx1823616178@outlook.com>
2023-12-15 11:46:57 +0800

    首页

--- src/layout/HomeLayout/componse/Navbar.jsx
+++ src/layout/HomeLayout/componse/Navbar.jsx
@@ -4,45 +4,46 @@ import '../style/index.scss'
 export function Navbar() {
     const [navbarList, setNavBarList] = useState([])
     const [isLoading, setIsLoading] = useState(false)
-
     useEffect(() => {
-        assembleNavList()
-    });
+        setNavBarList(assembleNavList())
+    }, []);
     /**
      * Init
      */
     const assembleNavList = () => {
         let arr = []
-        arr.push(
-            {title: "首页", url: ""},
-        )
+        arr.push({title: "首页", url: ""})
         arr.push({title: "知识图谱", url: ""})
         arr.push({title: "知识", url: ""})
         arr.push({title: "文献", url: ""})
         arr.push({title: "图表", url: ""})
         arr.push({title: "帮助中心", url: ""})
-        setNavBarList(arr)
+        return arr
     }
 
 
-    if (isLoading) {
-        return <div>loading...</div>
-    } else {
-        return (
-            <div>
-                <div className={"navbar_container"}>
-                    <div className={"navbar_logo"}></div>
-                    <div className={"nav_list"}>
-                        {navbarList.map((res, index) => {
-                            return <div className={"nav_list_li"} key={index}>
-                                <div className={"nav_list_title"}>{res.title}</div>
-                            </div>
-                        })}
+    return (
+        <div>
+            <div className={"navbar_container"}>
+                <div className={"navbar_logo"}></div>
+                <div className={"nav_list"}>
+                    {navbarList.map((res, index) => {
+                        return <div className={"nav_list_li"} key={index}>
+                            <div className={"nav_list_title"}>{res.title}</div>
+                        </div>
+                    })}
+                </div>
+                <div className={"nav_login_container"}>
+                    <div className={"nav_login_user_line"}></div>
+                    <div className={"nav_cont"}>
+                        <div className={"nav_login_user_icon"}></div>
+                        <div className={"nav_login_user_title"}>Admin</div>
                     </div>
+
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 
